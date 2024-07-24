6a22299ce727a135c50fab2b49379f0002df5d10
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-07-24 18:32:50 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -10,11 +10,11 @@ with st.sidebar:
 #    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
 #    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("📝 File Q&A with Anthropic")
-uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
+st.title("📝 Análise do Edital")
+uploaded_file = st.file_uploader("Carregue o arquivo com o edital", type=("pdf","docx","txt", "md"))
 question = st.text_input(
-    "Ask something about the article",
-    placeholder="Can you give me a short summary?",
+    "Faça um questionamento",
+    placeholder="Pode fornecer um sumário?",
     disabled=not uploaded_file,
 )
 
