fe1974edf54a2879c414b30c344c03e70b6c35c5
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-07-24 18:34:49 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -5,7 +5,7 @@
 import streamlit as st
 import anthropic
 
-with st.sidebar:
+# with st.sidebar:
 #    anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
 #    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
 #    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
