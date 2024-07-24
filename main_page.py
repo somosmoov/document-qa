1880c5f539326e2b361d8e89da15c6fce1bc3d9d
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-07-24 18:24:33 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -7,8 +7,8 @@ import anthropic
 
 with st.sidebar:
     anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+#    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
+#    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("📝 File Q&A with Anthropic")
 uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
