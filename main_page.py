c47f500796d40ba56951468c3be4a91bd7500669
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-07-24 18:37:58 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -5,11 +5,6 @@ nta  # import streamlit as st
 import streamlit as st
 import anthropic
 
-# with st.sidebar:
-#    anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
-#    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
-#    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
 st.title("📝 Análise do Edital")
 uploaded_file = st.file_uploader("Carregue o arquivo com o edital", type=("pdf","docx","txt", "md"))
 question = st.text_input(
@@ -17,7 +12,5 @@ question = st.text_input(
     placeholder="Pode fornecer um sumário?",
     disabled=not uploaded_file,
 )
-
-
-    st.write("### Pergunta")
-    #st.write(response.completion)
+st.write("### Pergunta")
+#st.write(response.completion)
