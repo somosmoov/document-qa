854bd208953d067de9d5baa20736ef82ecdf67c0
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-07-25 13:07:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ def logout():
         st.session_state.logged_in = False
         st.rerun()
 
+st.session_state
 def carrega():
     st.title("📝 Carregue o Edital")
     uploaded_file = st.file_uploader("Carregue o arquivo com o edital", type=("pdf","docx","txt", "md"))#,accept_multiple_files=True)
