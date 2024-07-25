cec30d9df18dceebacdcf142ae71f4ed4d2d83c2
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-07-25 11:52:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def logout():
 
 def carrega():
     st.title("📝 Carregue o Edital")
-    uploaded_file = st.file_uploader("Carregue o arquivo com o edital", type=("pdf","docx","txt", "md"),accept_multiple_files=True)
+    uploaded_file = st.file_uploader("Carregue o arquivo com o edital", type=("pdf","docx","txt", "md"))#,accept_multiple_files=True)
     question = st.text_input(
         "Faça um questionamento",
         placeholder="Pode fornecer um sumário?",
