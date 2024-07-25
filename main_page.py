3cacd4fda2e56a3358da9b4436524078f0a69ec0
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-07-24 22:13:56 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 st.markdown("# Moov - Analisador de Editais 🎈")
-st.sidebar.markdown("# Início 🎈")
-
+#st.sidebar.markdown("# Início 🎈")
+pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
+pg.run()
 st.title("📝 Análise do Edital")
 uploaded_file = st.file_uploader("Carregue o arquivo com o edital", type=("pdf","docx","txt", "md"))
 question = st.text_input(
