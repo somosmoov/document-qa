045f7568bb70f0b89a0c5bd63a02c34156fd19e2
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-07-29 16:11:11 -0300

    Delete pages directory

--- pages/page_2.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.markdown("# Página 2 ❄️")
-st.sidebar.markdown("# Página 2 ❄️")
--- pages/page_4.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.markdown("# Page 4 🎉")
-st.sidebar.markdown("# Page 4 🎉")
