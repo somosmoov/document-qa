25b3ce93f98d9606ec5db79dbe86d1c0a4de3307
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-08-01 16:57:40 -0300

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,10 @@ logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 bugs = st.Page("reports/bugs.py", title="Log de erros", icon=":material/bug_report:")#, default=True)
 alerts = st.Page("reports/alerts.py", title="Alertas", icon=":material/notification_important:")#, default=True)
 
-upload = st.Page("tools/upload.py", title="Carrega Edital", icon=":material/upload:", default=True)
+upload = st.Page("tools/upload.py", title="Carrega Edital", icon=":material/upload:")#, default=True)
 search = st.Page("tools/search.py", title="Pesquisas", icon=":material/search:")#, default=True)
 history = st.Page("tools/history.py", title="Historico", icon=":material/history:")#, default=True)
+qa = st.Page("tools/qa.py", title="Carrega Edital", icon=":material/upload:", default=True)
 
 # Substitua 'SUA_API_KEY' pela sua chave da API do Cohere
 api_key = st.secrets["api_cohere"]
@@ -49,7 +50,7 @@ if st.session_state.logged_in:
     pg = st.navigation(
         {
             "Acesso": [logout_page],
-            "Ferramentas": [upload],# search],history],
+            "Ferramentas": [qa]#upload],# search],history],
             #"Relatórios": [bugs, alerts],
             
         }
