a60d8f89d4cbc64294d2eba0fa1edfbedc7a944c
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-10-15 16:15:53 -0300

    Update qa.py

--- tools/qa.py
+++ tools/qa.py
@@ -11,7 +11,7 @@ from azure.keyvault.secrets import SecretClient
 from langchain_community.document_loaders import PDFMinerLoader
 from langchain_community.document_loaders import PyMuPDFLoader
 import pymupdf4llm
-import chardet
+import chardet 
 
 
 KEY_VAULT_URL = "https://keyvaultdesen.vault.azure.net/"
